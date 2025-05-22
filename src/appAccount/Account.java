d4fcc50317d17e3d7046b3d1696150a10b01338d
Erika Fernanda <e.ferlino@gmail.com>
2025-05-22 13:55:49 -0300

    Adicionando funções de saque, deposito e tranferencias

--- src/appAccount/Account.java
+++ src/appAccount/Account.java
@@ -6,11 +6,29 @@ public class Account {
     String accountOwner;
     double balance;
 
+    boolean withdraw(double amount) {
+       if (amount > 0) {
+           balance -= amount;
+           return true;
+       }
+       return false;
+    }
+
+    boolean deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    void transfer (double amount, Account targetAccount) {
+        withdraw(amount);
+        targetAccount.deposit(amount);
+    }
+
     void printBalance () {
         System.out.println("Balance: R$ " + balance);
     }
 
-    void deposit(double amount) {
-        balance += amount;
-    }
 }
--- src/appAccount/App2.java
+++ src/appAccount/App2.java
@@ -6,8 +6,18 @@ public class App2 {
 
         Account a = new Account();
         a.balance = 100;
-        a.deposit(100);
+
+        if (a.deposit(-100)) {
+            System.out.println("Ops, depósito inválido");
+        }
+        a.printBalance();
+
         a.deposit(300);
+
+        if (!a.withdraw(-50)){
+            System.out.println("Ops, Saque inválido");
+        }
+
         a.printBalance();
     }
 }
